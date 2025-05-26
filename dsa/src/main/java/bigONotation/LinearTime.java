dbdda30fa0837b2eca07bcb831c294503cac5d98
Rahul Pandita <rpandita1999@outlook.com>
2025-05-26 11:31:02 +0530

    algorithm with 2 different inputs of size n and m; O(m+n) simplified to O(n) since still linear growth

--- dsa/src/main/java/bigONotation/LinearTime.java
+++ dsa/src/main/java/bigONotation/LinearTime.java
@@ -2,17 +2,17 @@ package bigONotation;
 
 public class LinearTime {
 
-    public void linearTimeOperation(int[] numbers) {
+    public void linearTimeOperation(int[] numbers, String[] words) {
         // O(n) -> the cost (number of operations) grows linearly in direct correlation
         // with increase in input size n
 
-        // O(2n) -> O(n)
+        // O(m+n) -> O(n) since runtime still increases linearly
         for (int num : numbers) { // O(n)
             System.out.println(num);
         }
 
-        for (int num : numbers) { // O(n)
-            System.out.println(num);
+        for (String s : words) { // O(m)
+            System.out.println(s);
         }
     }
 }
