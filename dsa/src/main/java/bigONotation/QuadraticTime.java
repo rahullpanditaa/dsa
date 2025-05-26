0fdcd4c9b116932452406a578e9a8ff5f801b739
Rahul Pandita <rpandita1999@outlook.com>
2025-05-26 11:41:52 +0530

    O(n + n^2) -> O(n^2)

--- dsa/src/main/java/bigONotation/QuadraticTime.java
+++ dsa/src/main/java/bigONotation/QuadraticTime.java
@@ -3,6 +3,13 @@ package bigONotation;
 public class QuadraticTime {
 
     public static void hello(int[] numbers) {
+
+        // O(n + n^2) -> concerned with approximation -> O(n^2)
+
+        // O(n)
+        for (int num : numbers) {
+            System.out.println(num);
+        }
         // O(n^2)
         for (int first : numbers) {  // O(n)
             for (int second : numbers) { // O(n)
