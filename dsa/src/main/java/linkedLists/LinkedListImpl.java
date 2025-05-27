849632a4c775b25378da7294fcf2ab615a839006
Rahul Pandita <rpandita1999@outlook.com>
2025-05-27 11:26:53 +0530

    call size() once to traverse one time

--- dsa/src/main/java/linkedLists/LinkedListImpl.java
+++ dsa/src/main/java/linkedLists/LinkedListImpl.java
@@ -126,9 +126,11 @@ public class LinkedListImpl {
 
     // convert linked list to a static array
     public int[] toArray() {
-        var array = new int[size()];
+        // call size once
+        var size = size();
+        var array = new int[size];
         var currentNode = first;
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             array[i] = currentNode.getValue();
             currentNode = currentNode.getNext();
         }
